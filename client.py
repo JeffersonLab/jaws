28ee59ba7775ded10ef6e925dabcc9355acb065b
slominskir <Ryan.Slominski@gmail.com>
2020-04-30 19:28:05 -0400

    Load initial state to filter out old messages, then continue monitoriing

--- client.py
+++ client.py
@@ -19,8 +19,14 @@ c = Consumer({
     'group.id': 'client.py'})
 
 def my_on_assign(consumer, partitions):
+    global highOffsets
+
+    highOffsets = {}
+
     for p in partitions:
         p.offset = OFFSET_BEGINNING
+        low, high = c.get_watermark_offsets(p)
+        highOffsets[p.topic] = high
     consumer.assign(partitions)
 
 c.subscribe(['active-alarms','shelved-alarms','alarms'], on_assign=my_on_assign)
@@ -29,21 +35,23 @@ active = {}
 shelved = {}
 alarms = {}
 
-while True:
-    try:
-        msg = c.poll(1.0)
+activeLoaded = False
+shelvedLoaded = False
+alarmsLoaded = False
 
-    except SerializerError as e:
-        print("Message deserialization failed for {}: {}".format(msg, e))
-        break
+class ContinueException(Exception):
+  pass
 
-    if msg is None:
-        continue
+def poll_msg(): 
+    global activeLoaded
+    global shelvedLoaded
+    global alarmsLoaded
+    global highOffsets
 
-    if msg.error():
-        print("AvroConsumer error: {}".format(msg.error()))
-        continue
+    msg = c.poll(1.0)
 
+    if msg is None or msg.error():
+      return [msg, None] 
 
     topic = msg.topic()
     key = msg.key().decode('utf-8')
@@ -51,16 +59,68 @@ while True:
 
     if topic == "alarms":
       alarms[key] = value
+
+      if msg.offset() + 1 == highOffsets["alarms"]:
+        alarmsLoaded = True
+
     elif topic == "shelved-alarms":
       shelved[key] = value
+
+      if msg.offset() + 1 == highOffsets["shelved-alarms"]:
+        shelvedLoaded = True
+
     elif topic == "active-alarms":
       active[key] = value
+
+      if msg.offset() + 1 == highOffsets["active-alarms"]:
+        activeLoaded = True
     else:
       print("Unknown topic {}", topic)
 
+    #print(topic, key, value)
+    return [msg, key]
 
-    print(topic, key, value)
+while True:
+  try:
+    msg, key = poll_msg()
+  except SerializerError as e:
+    print("Message deserialization failed for {}".format(e))
+    break
 
+  if msg is None:
+    continue
 
+  if msg.error():
+    print("Continuing {}: {}".format(msg, msg.error()))
+    continue
+
+  if alarmsLoaded and shelvedLoaded and activeLoaded:
+    break
+
+# At this point the inital flurry of messages have been read up to the high water mark offsets read moments ago.  Now we can report somewhat up-to-date snapshot of system state and start monitoring for anything that has happend since reading high water mark or anything coming in the furure
+print("Initial State:")
+for key in active:
+   if active.get(key):
+     print(key, active.get(key), "shelved:", shelved.get(key), "info:", alarms.get(key))
+
+print("Continuing to monitor: ")
+while True:
+  try:
+    msg, key  = poll_msg()
+  except SerializerError as e:
+    print("Message deserialization failed for {}".format(e))
+    break
+  
+  if msg is None:
+    continue
+
+  if msg.error():
+    print("Continuing {}: {}".format(msg, msg.error()))
+    continue 
+
+  if active.get(key):
+    print(key, active.get(key), "shelved:", shelved.get(key), "info:", alarms.get(key))
+  else:
+    print(key, "No longer alarming")
 
 c.close()
