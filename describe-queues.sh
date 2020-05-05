64578564c148602640ff019f58177a449b60e32a
slominskir <Ryan.Slominski@gmail.com>
2020-05-05 17:22:02 -0400

    KAFKA_HOME env

--- describe-queues.sh
+++ describe-queues.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
+KAFKA_HOME=${KAFKA_HOME:=/opt/kafka/pro}
 BOOTSTRAP_SERVERS=${BOOTSTRAP_SERVERS:=localhost:9092}
 
-cd -P /opt/kafka/pro
+cd -P $KAFKA_HOME
 
 bin/kafka-consumer-groups.sh --bootstrap-server $BOOTSTRAP_SERVERS --all-groups --describe
--- list-topics.sh
+++ list-topics.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
+KAFKA_HOME=${KAFKA_HOME:=/opt/kafka/pro}
 ZOOKEEPER=${ZOOKEEPER:=localhost:2181}
 
-cd -P /opt/kafka/pro
+cd -P $KAFKA_HOME
 
 bin/kafka-topics.sh --list --zookeeper $ZOOKEEPER
