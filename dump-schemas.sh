f8cf73b7ebd9901d4f52b7521b01872595b3a7d5
slominskir <Ryan.Slominski@gmail.com>
2020-05-05 17:31:16 -0400

    Avoid hard coding paths...

--- dump-schemas.sh
+++ dump-schemas.sh
@@ -2,7 +2,7 @@
 
 SCHEMA_REGISTRY=${SCHEMA_REGISTRY:=http://localhost:8081}
 
-SCRIPT_DIR=/opt/kafka/scripts
+SCRIPT_DIR=`dirname "$(readlink -f "$0")"`
 DMP_DIR=${DMP_DIR:=/tmp/schema-cache}
 
 cd $SCRIPT_DIR
