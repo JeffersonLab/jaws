37105c3d11b600dcbb12ad8e03a77d64adf755e4
Ryan Slominski <Ryan.Slominski@gmail.com>
2020-09-29 13:19:56 -0400

    Update create-alarm-system-topics.sh

--- examples/kafka-scripts/create-alarm-system-topics.sh
+++ examples/kafka-scripts/create-alarm-system-topics.sh
@@ -7,7 +7,7 @@ cd -P $KAFKA_HOME
 
 bin/kafka-topics.sh --bootstrap-server $KAFKA_BOOTSTRAP_SERVER \
              --create \
-             --topic alarms \
+             --topic registered-alarms \
              --config cleanup.policy=compact \
              --config min.cleanable.dirty.ratio=0.01 \
              --config delete.retention.ms=100 \
