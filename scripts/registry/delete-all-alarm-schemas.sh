e881fcc50f51481c4a4f5cfee7de3d8f7d8c5d41
Ryan <Ryan.Slominski@gmail.com>
2021-01-26 16:07:33 -0500

    Script to clear out schemas

---
+++ scripts/registry/delete-all-alarm-schemas.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+SCHEMA_REGISTRY=${SCHEMA_REGISTRY:=http://registry:8081}
+
+curl -s -X DELETE -w "\n" $SCHEMA_REGISTRY/subjects/registered-alarms-value | jq -r '.[]'
+curl -s -X DELETE -w "\n" $SCHEMA_REGISTRY/subjects/registered-alarms-value?permanent=true | jq -r '.[]'
+
+
+curl -s -X DELETE -w "\n" $SCHEMA_REGISTRY/subjects/shelved-alarms-value | jq -r '.[]'
+curl -s -X DELETE -w "\n" $SCHEMA_REGISTRY/subjects/shelved-alarms-value?permanent=true | jq -r '.[]'
+
+
+curl -s -X DELETE -w "\n" $SCHEMA_REGISTRY/subjects/active-alarms-value | jq -r '.[]'
+curl -s -X DELETE -w "\n" $SCHEMA_REGISTRY/subjects/active-alarms-value?permanent=true | jq -r '.[]'
