395772545db49be7c5967ffe2fc32cdd04492962
ryans <ryans@jlab.org>
2021-02-01 18:08:01 -0500

    Not used

--- scripts/broker/describe-queues.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-[ -z "$KAFKA_HOME" ] && echo "KAFKA_HOME environment required" && exit 1;
-
-[ -z "$BOOTSTRAP_SERVER" ] && echo "BOOTSTRAP_SERVER environment required" && exit 1;
-
-$KAFKA_HOME/bin/kafka-consumer-groups.sh --bootstrap-server $BOOTSTRAP_SERVER --all-groups --describe
-
