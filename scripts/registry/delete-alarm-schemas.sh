216e35e42cb63aea4a17d3950ec64c7a6fe1667e
ryans <ryans@jlab.org>
2021-02-01 15:21:01 -0500

    Delete registered-alarms-key too

--- scripts/registry/delete-alarm-schemas.sh
+++ scripts/registry/delete-alarm-schemas.sh
@@ -2,6 +2,9 @@
 
 SCHEMA_REGISTRY=${SCHEMA_REGISTRY:=http://registry:8081}
 
+curl -s -X DELETE -w "\n" $SCHEMA_REGISTRY/subjects/registered-alarms-key | jq -r '.[]'
+curl -s -X DELETE -w "\n" $SCHEMA_REGISTRY/subjects/registered-alarms-key?permanent=true | jq -r '.[]'
+
 curl -s -X DELETE -w "\n" $SCHEMA_REGISTRY/subjects/registered-alarms-value | jq -r '.[]'
 curl -s -X DELETE -w "\n" $SCHEMA_REGISTRY/subjects/registered-alarms-value?permanent=true | jq -r '.[]'
 
