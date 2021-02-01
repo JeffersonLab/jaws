80bcf5560f635c177f996d6c5aad95e9c17e802f
ryans <ryans@jlab.org>
2021-02-01 14:17:19 -0500

    Use Python API so that Java broker jars are not required

---
+++ scripts/broker/create-alarms-topics.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+from confluent_kafka.admin import AdminClient, NewTopic
+from confluent_kafka import KafkaException
+
+import os
+
+bootstrap_servers = os.environ.get('BOOTSTRAP_SERVERS', 'localhost:9092')
+
+a = AdminClient({'bootstrap.servers': bootstrap_servers})
+
+topics = ['registered-alarms', 'active-alarms', 'shelved-alarms']
+
+new_topics = [NewTopic(topic, num_partitions=1, replication_factor=1, config={"cleanup.policy": "compact"}) for topic in topics]
+
+fs = a.create_topics(new_topics, operation_timeout=15)
+
+for topic, f in fs.items():
+    try:
+        f.result()
+        print("Topic {} created".format(topic))
+    except Exception as e:
+        print("Failed to create topic {}: {}".format(topic, e))
---
+++ scripts/broker/delete-alarms-topics.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+from confluent_kafka.admin import AdminClient
+from confluent_kafka import KafkaException
+
+import os
+
+bootstrap_servers = os.environ.get('BOOTSTRAP_SERVERS', 'localhost:9092')
+
+a = AdminClient({'bootstrap.servers': bootstrap_servers})
+
+topics = ['registered-alarms', 'active-alarms', 'shelved-alarms']
+
+fs = a.delete_topics(topics, operation_timeout=15)
+
+for topic, f in fs.items():
+    try:
+        f.result()  # The result itself is None
+        print("Topic {} deleted".format(topic))
+    except Exception as e:
+        print("Failed to delete topic {}: {}".format(topic, e))
