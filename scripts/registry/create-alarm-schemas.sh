5e8167872beb838221449c1da89bcf0acb42bd63
ryans <ryans@jlab.org>
2021-02-01 15:03:24 -0500

    Initial commit of script

---
+++ scripts/registry/create-alarm-schemas.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+SCHEMA_REGISTRY=${SCHEMA_REGISTRY:=http://registry:8081}
+
+SCRIPT_DIR=`dirname "$(readlink -f "$0")"`
+
+data="$(cat $SCRIPT_DIR/../../schemas/registered-alarms-value.avsc)"
+data="{\"schema\":
+$data
+}"
+echo "$data"
+exit
+curl -s -X POST -H "Content-Type: application/vnd.schemaregistry.v1+json" \
+            --data "$data" \
+            $SCHEMA_REGISTRY/subjects/registered-alarms-value/versions
+
+
+data="$(cat $SCRIPT_DIR/../../schemas/active-alarms-value.avsc)"
+data="{\"schema\":
+$data
+}"
+curl -s -X POST -H "Content-Type: application/vnd.schemaregistry.v1+json" \
+            --data "$data" \
+            $SCHEMA_REGISTRY/subjects/active-alarms-value/versions
+
+data="$(cat $SCRIPT_DIR/../../schemas/shelved-alarms-value.avsc)"
+data="{\"schema\":
+$data
+}"
+curl -s -X POST -H "Content-Type: application/vnd.schemaregistry.v1+json" \
+            --data "$data" \
+            $SCHEMA_REGISTRY/subjects/shelved-alarms-value/versions
