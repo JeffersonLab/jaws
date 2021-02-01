b2034998e808e06cef78766301c78f4a37003aa4
ryans <ryans@jlab.org>
2021-02-01 15:03:43 -0500

    rename to be consistent

--- scripts/registry/delete-alarm-schemas.sh
+++ scripts/registry/delete-alarm-schemas.sh
@@ -5,10 +5,8 @@ SCHEMA_REGISTRY=${SCHEMA_REGISTRY:=http://registry:8081}
 curl -s -X DELETE -w "\n" $SCHEMA_REGISTRY/subjects/registered-alarms-value | jq -r '.[]'
 curl -s -X DELETE -w "\n" $SCHEMA_REGISTRY/subjects/registered-alarms-value?permanent=true | jq -r '.[]'
 
+curl -s -X DELETE -w "\n" $SCHEMA_REGISTRY/subjects/active-alarms-value | jq -r '.[]'
+curl -s -X DELETE -w "\n" $SCHEMA_REGISTRY/subjects/active-alarms-value?permanent=true | jq -r '.[]'
 
 curl -s -X DELETE -w "\n" $SCHEMA_REGISTRY/subjects/shelved-alarms-value | jq -r '.[]'
 curl -s -X DELETE -w "\n" $SCHEMA_REGISTRY/subjects/shelved-alarms-value?permanent=true | jq -r '.[]'
-
-
-curl -s -X DELETE -w "\n" $SCHEMA_REGISTRY/subjects/active-alarms-value | jq -r '.[]'
-curl -s -X DELETE -w "\n" $SCHEMA_REGISTRY/subjects/active-alarms-value?permanent=true | jq -r '.[]'
