b1fb33ccfae43809cdc4495f31d8a4608ce36315
ryans <ryans@jlab.org>
2021-05-04 10:57:03 -0400

    Keep topic param viable

--- scripts/client/list-active.py
+++ scripts/client/list-active.py
@@ -10,14 +10,12 @@ from confluent_kafka.serialization import StringDeserializer
 from jlab_jaws.avro.subject_schemas.serde import ActiveAlarmSerde
 from jlab_jaws.eventsource.table import EventSourceTable
 
-
 bootstrap_servers = os.environ.get('BOOTSTRAP_SERVERS', 'localhost:9092')
 
 sr_conf = {'url': os.environ.get('SCHEMA_REGISTRY', 'http://localhost:8081')}
 schema_registry_client = SchemaRegistryClient(sr_conf)
 
 key_deserializer = StringDeserializer()
-
 value_deserializer = ActiveAlarmSerde.deserializer(schema_registry_client)
 
 
@@ -65,7 +63,7 @@ def handle_state_update(records):
 def list_records():
     ts = time.time()
 
-    config = {'topic': 'active-alarms',
+    config = {'topic': params.topic,
               'monitor': params.monitor,
               'bootstrap.servers': bootstrap_servers,
               'key.deserializer': key_deserializer,
