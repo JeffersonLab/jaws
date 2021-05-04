49c089c093a78761c1878dec59749f9df31c165e
ryans <ryans@jlab.org>
2021-05-04 13:27:07 -0400

    Use version 0.0.10 of jaws-libp

--- scripts/client/set-active.py
+++ scripts/client/set-active.py
@@ -7,14 +7,11 @@ import click
 
 from confluent_kafka import SerializingProducer
 from confluent_kafka.schema_registry import SchemaRegistryClient
-from confluent_kafka.schema_registry.avro import AvroSerializer
 from confluent_kafka.serialization import StringSerializer
+from jlab_jaws.avro.subject_schemas.entities import ActiveAlarm, SimpleAlarming, EPICSAlarming, NoteAlarming, EPICSSEVR, \
+    EPICSSTAT
+from jlab_jaws.avro.subject_schemas.serde import ActiveAlarmSerde
 
-scriptpath = os.path.dirname(os.path.realpath(__file__))
-projectpath = scriptpath + '/../../'
-
-with open(projectpath + '/config/subject-schemas/active-alarms-value.avsc', 'r') as file:
-    value_schema_str = file.read()
 
 def delivery_report(err, msg):
     """ Called once for each message produced to indicate delivery result.
@@ -30,8 +27,7 @@ sr_conf = {'url':  os.environ.get('SCHEMA_REGISTRY', 'http://localhost:8081')}
 schema_registry_client = SchemaRegistryClient(sr_conf)
 
 key_serializer = StringSerializer()
-
-value_serializer = AvroSerializer(schema_registry_client, value_schema_str)
+value_serializer = ActiveAlarmSerde.serializer(schema_registry_client)
 
 producer_conf = {'bootstrap.servers': bootstrap_servers,
                  'key.serializer': key_serializer,
@@ -53,9 +49,12 @@ def send() :
 
 @click.command()
 @click.option('--unset', is_flag=True, help="present to clear an alarm, missing to set active")
+@click.option('--note', help="The note (only for NoteAlarming)")
+@click.option('--sevr', type=click.Choice(EPICSSEVR._member_names_), help="The sevr (only for EPICSAlarming)")
+@click.option('--stat', type=click.Choice(EPICSSTAT._member_names_), help="The stat (only for EPICSAlarming)")
 @click.argument('name')
 
-def cli(unset, name):
+def cli(unset, note, stat, sevr, name):
     global params
 
     params = types.SimpleNamespace()
@@ -64,8 +63,14 @@ def cli(unset, name):
 
     if unset:
       params.value = None
+    elif sevr and stat:
+        msg = EPICSAlarming(sevr, stat)
+    elif note:
+        msg = NoteAlarming(note)
     else:
-      params.value = {"msg": {}}
+        msg = SimpleAlarming()
+
+    params.value = ActiveAlarm(msg)
 
     send()
 
