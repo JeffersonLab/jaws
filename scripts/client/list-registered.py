ab537d5803162d6eb7724fce7a4a706c7a069766
ryans <ryans@jlab.org>
2021-05-17 10:56:34 -0400

    Change rows should look different so removing format todo

--- scripts/client/list-registered.py
+++ scripts/client/list-registered.py
@@ -106,7 +106,7 @@ def handle_initial_state(records):
 def handle_state_update(record):
     row = get_row(record)
     if row is not None:
-        print(row)  # TODO: format with a row template!
+        print(row)
 
 
 def list_records():
