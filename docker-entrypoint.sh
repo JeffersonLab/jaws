f18dcb480aa2f2ab4fb5ed9e47fdcc88e68595fb
slominskir <Ryan.Slominski@gmail.com>
2021-05-19 14:02:36 -0400

    Registered Classes support

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -14,10 +14,45 @@ echo "---------------------------------------"
 /scripts/registry/create-alarm-schemas.py
 
 echo "----------------------------------------------------------------"
-echo "Step 3: Adding alarm definitions to the registered-alarms topic "
+echo "Step 3: Adding class definitions to the registered-classes topic "
+echo "----------------------------------------------------------------"
+if [[ -z "${CLASS_DEFINITIONS}" ]]; then
+  echo "No class definitions specified"
+elif [[ -f "$CLASS_DEFINITIONS" ]]; then
+  echo "Attempting to setup class definitions from file $CLASS_DEFINITIONS"
+  /scripts/client/set-registered.py --editclass --file "$CLASS_DEFINITIONS"
+else
+  echo "Attempting to setup class definitions"
+  IFS=','
+  read -a definitions <<< "$CLASS_DEFINITIONS"
+  for defStr in "${definitions[@]}";
+    do
+      IFS='|'
+      read -a def <<< "$defStr"
+      name="${def[0]}"
+      location="${def[1]}"
+      category="${def[2]}"
+      priority="${def[3]}"
+      rationale="${def[4]}"
+      correctiveaction="${def[5]}"
+      pointofcontactusername="${def[6]}"
+      screenpath="${def[7]}"
+      echo "Creating class definition ${name} ${location}" "${category}" "${priority}" \
+          "${rationale}" "${correctiveaction}" "${pointofcontactusername}" "${screenpath}"
+      /scripts/client/set-registered.py --editclass "${name}" --location "${location}" --category "${category}" \
+          --priority "${priority}" --rationale "${rationale}" --correctiveaction "${correctiveaction}" \
+          --pointofcontactusername "${pointofcontactusername}" --screenpath "${screenpath}"
+    done
+fi
+
+echo "----------------------------------------------------------------"
+echo "Step 4: Adding alarm definitions to the registered-alarms topic "
 echo "----------------------------------------------------------------"
 if [[ -z "${ALARM_DEFINITIONS}" ]]; then
   echo "No alarm definitions specified"
+elif [[ -f "$ALARM_DEFINITIONS" ]]; then
+  echo "Attempting to setup alarm definitions from file $ALARM_DEFINITIONS"
+  /scripts/client/set-registered.py --file "$ALARM_DEFINITIONS"
 else
   echo "Attempting to setup alarm definitions"
   IFS=','
