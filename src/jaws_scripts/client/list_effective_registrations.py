f3d65913dae2aeebb7c4dd4979f549e5b4e84889
ryans <ryans@jlab.org>
2022-04-25 16:02:16 -0400

    Remove CATEGORIES filter

--- src/jaws_scripts/client/list_effective_registrations.py
+++ src/jaws_scripts/client/list_effective_registrations.py
@@ -10,42 +10,31 @@ from jaws_libp.clients import EffectiveRegistrationConsumer, CategoryConsumer
 
 
 # pylint: disable=too-few-public-methods
-class ClassAndCategoryFilter:
+class ClassFilter:
     """
-        Filter class and category messages
+        Filter class messages
     """
-    def __init__(self, category, alarm_class):
-        self._category = category
+    def __init__(self, alarm_class):
         self._alarm_class = alarm_class
 
     # pylint: disable=unused-argument
     def filter_if(self, key, value):
         """
-            Filter out messages unless the class and category matches the provided class and category
+            Filter out messages unless the class natches the provided class
         """
-        return (self._category is None or (value is not None and self._category == value.category)) and \
-               (self._alarm_class is None or (value is not None and self._alarm_class == value.alarm_class))
+        return self._alarm_class is None or (value is not None and self._alarm_class == value.alarm_class)
 
-
-CATEGORIES = []
-
-if __name__ == "__main__":
-    cat_consumer = CategoryConsumer('list_classes.py')
-    CATEGORIES = cat_consumer.get_keys_then_done()
 
 # pylint: disable=duplicate-code,missing-function-docstring,no-value-for-parameter
 @click.command()
 @click.option('--monitor', is_flag=True, help="Monitor indefinitely")
 @click.option('--nometa', is_flag=True, help="Exclude audit headers and timestamp")
 @click.option('--export', is_flag=True, help="Dump records in AVRO JSON format")
-@click.option('--category', type=click.Choice(CATEGORIES),
-              help="Only show registrations in the specified category (Options queried on-demand from "
-                   "alarm-categories topic)")
 @click.option('--alarm_class', help="Only show registrations in the specified class")
-def list_effective_registrations(monitor, nometa, export, category, alarm_class) -> None:
+def list_effective_registrations(monitor, nometa, export, alarm_class) -> None:
     consumer = EffectiveRegistrationConsumer('list_effective_registrations.py')
 
-    filter_obj = ClassAndCategoryFilter(category, alarm_class)
+    filter_obj = ClassFilter(alarm_class)
 
     consumer.consume_then_done(monitor, nometa, export, filter_obj.filter_if)
 
