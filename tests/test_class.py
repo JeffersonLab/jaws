4a7ab67a0100c97bc357db99c548c52e9559b10c
slominskir <ryan.slominski@gmail.com>
2022-04-28 10:16:33 -0400

    Updated tests

--- tests/test_class.py
+++ tests/test_class.py
@@ -1,6 +1,8 @@
 import os
 
+from click import Choice
 from click.testing import CliRunner
+from jaws_libp.avro.serde import ClassSerde
 from jaws_libp.entities import AlarmClass, AlarmPriority
 from jaws_scripts.client.list_classes import list_classes
 from jaws_scripts.client.set_class import set_class
@@ -14,17 +16,20 @@ def test_class():
 
     runner = CliRunner()
 
+    set_class.params[2].type = Choice([category])
+
     # Set
-    # result = runner.invoke(set_class, [class_name,
-    #                                   '--category', alarm_class.category,
-    #                                   '--priority', alarm_class.priority.name,
-    #                                   '--rationale', alarm_class.rationale,
-    #                                   '--correctiveaction', alarm_class.corrective_action,
-    #                                   '--pointofcontactusername', alarm_class.point_of_contact_username])
-    #print(result.output)
-    #assert result.exit_code == 0
+    result = runner.invoke(set_class, [class_name,
+                                       '--category', alarm_class.category,
+                                       '--priority', alarm_class.priority.name,
+                                       '--rationale', alarm_class.rationale,
+                                       '--correctiveaction', alarm_class.corrective_action,
+                                       '--pointofcontactusername', alarm_class.point_of_contact_username])
+    assert result.exit_code == 0
 
     # Get
     result = runner.invoke(list_classes, ['--export'])
     assert result.exit_code == 0
-    #assert result.output == class_name + '={"parent": null}\n'
+
+    class_serde = ClassSerde(None)
+    assert result.output == class_name + '=' + class_serde.to_json(alarm_class) + '\n'
