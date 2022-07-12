40b007b7dc6280ae6c7986f65911a5c5068a51ad
slominskir <ryan.slominski@gmail.com>
2022-07-12 17:13:31 -0400

    v4.5.3

--- docsrc/source/conf.py
+++ docsrc/source/conf.py
@@ -10,7 +10,7 @@ copyright = '2022, Ryan Slominski'
 author = 'Ryan Slominski'
 
 # The full version, including alpha/beta/rc tags
-release = '4.5.2'
+release = '4.5.3'
 
 
 # -- General configuration ---------------------------------------------------
